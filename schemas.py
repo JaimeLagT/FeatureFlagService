c94c837f08e83c4b0c01ac7a1cd17fb5544785a9
JLagT <jrl6253@psu.edu>
2026-02-12 15:14:05 -0500

    [feat]: Added Settings create class in schemas

--- schemas.py
+++ schemas.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel
 class FlagCreate(BaseModel):
     name: str
     description: str | None = None
+
+class SettingsCreate(BaseModel):
+    status: bool
+    environment: str
